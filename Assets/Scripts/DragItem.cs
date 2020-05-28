255fd6038328c5fd94a3bd53b43736685fc74a61
Artem Shaganov <kilosoft@mail.ru>
2020-05-28 19:31:49 +0300

    First commit and Swap support

---
+++ Assets/Scripts/DragItem.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static DragItem dragItem;
+    public Transform currentSlot;
+    private Vector3 startPosition;
+    private Transform startParrent;
+    private CanvasGroup canvasGroup;
+    private RectTransform dragLayer;
+    private Transform slot;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        dragLayer = GameObject.FindGameObjectWithTag("DragLayer").GetComponent<RectTransform>();
+        currentSlot = transform.parent;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        slot = null;
+        dragItem = this;
+        startPosition = transform.position;
+        startParrent = transform.parent;
+        transform.SetParent(dragLayer);
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragItem = null;
+        canvasGroup.blocksRaycasts = true;
+        if (slot == null)
+        {
+            transform.SetParent(startParrent);
+            transform.position = startPosition;
+        }
+        slot = null;
+    }
+
+    public void SetItemToSlot(Transform slot)
+    {
+        this.slot = slot;
+        transform.SetParent(slot);
+        currentSlot = slot;
+        transform.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/DropItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropItem : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        var item = DragItem.dragItem;
+        var childrens = transform.GetComponentsInChildren<DragItem>();
+        if (item != null && childrens.Length == 0)
+        {
+            item.SetItemToSlot(transform);
+        }
+        else if (item != null && childrens.Length > 0)
+        {
+            var slot = item.currentSlot;
+            childrens[0].SetItemToSlot(slot);
+            item.SetItemToSlot(transform);
+        }
+    }
+}
